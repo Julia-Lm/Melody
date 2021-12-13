33e82394cf8b3d745c15bd91237be7b81cf0d570
Julia-Lm <63656237+Julia-Lm@users.noreply.github.com>
2021-12-13 23:18:54 +0200

    create main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
     var currentFloor = 2;
     var floorPath = $('.home-image path');
-    var counterText = $('.counter-text');
+    var counterText = $('.counter');
     var counterUp = $('.counter-arrow-up');
     var counterDown = $('.counter-arrow-down');
+    var modal = $('.modal');
+    var modalClouseButton = $('.modal-clouse-button');
+    var flatPath = $('.flats path');
+    var viewFlatsBtn = $('.view-flats');
+    var flatItem = $('.flat-item');
+    var flatlLnk = $('.flat-link');
 
     floorPath.on('mouseover', function(){
         floorPath.removeClass("current-floor");
@@ -31,5 +37,36 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
+    });
+
+    function toggleModal(){
+        modal.toggleClass('is-open');
+    }
+
+    floorPath.on('click', toggleModal);
+
+    modalClouseButton.on('click', toggleModal);
+
+    viewFlatsBtn.on('click', toggleModal);
+
+    flatItem.on('mouseover', function(){
+        var flat = $(this).index();
+        flatPath.removeClass("current-flat");
+        $(`[data-flat=${flat}]`).toggleClass('current-flat');
+        flatlLnk.removeClass("is-active");
+    });
+
+    flatPath.on('mouseover', function(){
+        flatPath.removeClass("current-flat");
+        var flat = $(this).attr('data-flat');
+        flatlLnk.removeClass("is-active");
+        flatItem.eq(flat).find('a').toggleClass('is-active');
+    });
+
+    flatPath.on('mouseout', function(){
+        flatlLnk.removeClass("is-active");
+    });
+    flatItem.on('mouseout', function(){
+        flatPath.removeClass("current-flat");
     });
 });
